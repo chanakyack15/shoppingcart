6cbe5e354f9754f51f670c7f73ed77590fda5a08
chanakya <ck150895@gmail.com>
2020-08-12 19:49:30 +0530

    add api for getting products by GET/products

---
+++ WebApplication1/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication1.Data;
+using WebApplication1.Data.Entities;
+
+namespace WebApplication1.Controllers
+{
+    [Route("GET/[Controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IRepo repository;
+        private readonly ILogger<ProductsController> logger;
+
+        public ProductsController(IRepo repository, ILogger<ProductsController> logger)
+        {
+            this.repository = repository;
+            this.logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Get() {
+            return Ok(repository.GetAllProducts());
+        }
+    }
+}
